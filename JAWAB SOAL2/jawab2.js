2f346e86faa80c34a75d82268df0f87fd9dca478
manshir65 <albarragans9786@gmail.com>
2021-07-30 20:49:36 +0700

    tugas

--- JAWAB SOAL2/jawab2.js
+++
@@ -1,6 +0,0 @@
-var JualMobil = {
-    iklan: function(Nama, Merek, Tahun) {
-        document.writeln("Bu, saya: " + Nama + ", Jual Mobil: " + Merek + ", tahun: " + Tahun + ", Bekas")
-    }
-}
-JualMobil.iklan("Barra", "Toyota", 2021)
---
+++ NO.2 LATIHAN4/jawab2.js
@@ -0,0 +1,12 @@
+function introduce(perkenalan) {
+    console.log("Nama saya: " + name + ",Umur: " + age + ",alamat saya:" + address + ",Dan saya punya hobby: " + hobby)
+}
+
+
+var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogyakarta";
+var hobby = "Gaming";
+
+var perkenalan = introduce(name, age, address, hobby);
+console.log(perkenalan);
